ad46b2ba6f35df43bdd4a49bbe5f84ba54685691
Siddharth Pathak <siddharth.pathak122@gmail.com>
2025-02-24 22:46:23 -0500

    changes to port

--- src/main/java/com/example/recipe/controller/RecipeController.java
+++ src/main/java/com/example/recipe/controller/RecipeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/recipes/v1/recipes")
 public class RecipeController {
